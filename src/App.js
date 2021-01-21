0a41f92007ddf9f98b305af076ee6ab354b22d7a
Evgeniia Kharanen <evgeniia.kharanen@itc-engineering.com>
2021-01-21 14:53:05 +0100

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = { count : 0 };
+
+  increment = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  decrement = () => {
+    this.setState({count: this.state.count - 1})
+  }
+  render () {
+    return <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                </a>
+                <div>{this.state.count}</div>
+                <button className="btn" onClick={this.increment}>Прибавить</button>
+                <button className="btn" onClick={this.decrement}>Убавить</button>
+            </header>
+          </div>
+  ;}
 }
 
 export default App;
